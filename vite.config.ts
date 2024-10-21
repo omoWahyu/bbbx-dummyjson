e280bad5ce40ba3bed5350ba57d78d9677308623
Wahyudi Chrisdianto <wahyu@mys-MacBook-Pro.local>
2024-10-21 15:06:51 +0700

    update: remove image optimizer plugin coz unsuported platform

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import VitePluginWebpCompress from "vite-plugin-webp-compress";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePluginWebpCompress()],
+	plugins: [react()],
 });
